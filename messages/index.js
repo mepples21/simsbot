e6709bf58083e9785b1a4b8d07e90e32b54e0e77
Michael Epping <michael@rvrwest.com>
2017-03-31 11:23:56 -0500

    Updated phrases to use Matt's phrases

--- messages/index.js
+++ messages/index.js
@@ -14,26 +14,14 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 
 var arrOfMagicSayings = [
-  "Signs point to yes.",
-  "Yes.",
-  "Reply hazy, try again.",
-  "Without a doubt.",
-  "My sources say no.",
-  "As I see it, yes.",
-  "You may rely on it.",
-  "Concentrate and ask again.",
-  "Outlook not so good.",
-  "It is decidedly so.",
-  "Better not tell you now.",
-  "Very doubtful.",
-  "Yes - definitely.",
-  "It is certain.",
-  "Cannot predict now.",
-  "Most likely.",
-  "Ask again later.",
-  "My reply is no.",
-  "Outlook good.",
-  "Don't count on it."
+  "Sweet!",
+  "Muthaf***a",
+  "Wanna listen to some Phish?",
+  "dude",
+  "You rock!",
+  "noice",
+  "gents...",
+  "I need to check with my wife."
 ]
 
 var randomIndex = Math.round(Math.random() * arrOfMagicSayings.length);
