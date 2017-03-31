c481821f65a58b0570fcc0cb6361afce5f502f07
Michael Epping <michael@rvrwest.com>
2017-03-31 12:16:57 -0500

    Removed undefined function

--- messages/index.js
+++ messages/index.js
@@ -25,7 +25,6 @@ var mattPhrases = [
   "I love Dave Matthews Band!"
 ]
 
-var randomIndex = undefined
 var randomIndex = mattPhrases[Math.floor(Math.random()*mattPhrases.length)];
 
 bot.dialog('/', function (session) {
