00b57e7e273d33f937dc2a633767ae38fc9a629d
Michael Epping <michael@rvrwest.com>
2017-03-31 12:15:15 -0500

    Added step to undefine the variable

--- messages/index.js
+++ messages/index.js
@@ -25,6 +25,7 @@ var mattPhrases = [
   "I love Dave Matthews Band!"
 ]
 
+var randomIndex = undefined
 var randomIndex = mattPhrases[Math.floor(Math.random()*mattPhrases.length)];
 
 bot.dialog('/', function (session) {
